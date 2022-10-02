558d6305ef63a50de176be6a94d0dbadccc44fb4
annas <annasan14052003@gmail.com>
2022-10-02 22:21:37 +0300

    Eiler added

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-void main() {
-	setlocale(LC_ALL, "rus");
-	cout << "gvaeg";
-	func();
+int main() {
+	int a = 0, b = 1, h = 0.1;
+
+	for (int i = a; i < b; i += h) {
+		cout << y(PI / 2 + i, i, 0.1) << "\n";
+	}
+
+	return EXIT_SUCCESS;
 }
